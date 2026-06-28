7ade1f3f80569e39df056d2424f5172938e09525
RMBLOGG <ramdanhidayah176@gmail.com>
2026-06-29 00:43:38 +0700

    add preview

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -220,7 +220,7 @@ export default function Home({ dataSource }: HomeProps) {
             <span className="px-2 py-0.5 rounded-full bg-[#f04438]/15 text-[#f04438] text-[9px] font-bold tracking-wider uppercase">20 New</span>
           </div>
         </div>
-        <div id="histats_counter" className="flex items-center justify-center min-w-[40px]" />
+        <div id="histats_counter" className="flex items-center justify-center" />
       </div>
 
       {/* SEARCH BAR */}
